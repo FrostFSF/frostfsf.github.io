0f621a701f80257ba12e520556adea8ab0ed69c4
CraftPur-Official <rinku.apnatourandtravel@gmail.com>
2023-05-11 14:52:11 +0530

    firstCommit

---
+++ to-do-list.js
@@ -0,0 +1,50 @@
+function addFunc(element) {
+    const textField = element.previousElementSibling;
+    const textFieldValue = textField.value;
+    
+    if(textFieldValue === '')
+    {
+        const alertPopUp = element.parentElement.parentElement.parentElement.nextElementSibling;
+        alertPopUp.classList.remove('inactive');
+    }
+    else {
+        const ulElement = document.querySelector('.myList');
+        const listElement = document.createElement('li');
+        const checkList = document.createElement('i');
+        const deleteIcon = document.createElement('i');
+        const iconDiv = document.createElement('div')
+
+        deleteIcon.classList.add('fa-solid');
+        deleteIcon.classList.add('fa-trash');
+        deleteIcon.setAttribute("onclick","deleteThisList(this)");
+        checkList.classList.add('fa-solid');
+        checkList.classList.add('fa-circle-check');
+        checkList.setAttribute("onclick","checkThisList(this)");
+
+        iconDiv.appendChild(checkList);
+        iconDiv.appendChild(deleteIcon);
+
+        listElement.innerText = textFieldValue;
+        listElement.appendChild(iconDiv);
+        
+        ulElement.append(listElement);
+
+        textField.value = "";
+    }
+}
+
+function checkThisList(element) {
+    const listElement = element.parentElement.parentElement;
+    listElement.classList.add('checked');
+    element.classList.add('inactive')
+}
+
+function deleteThisList(element) {
+    const listElement = element.parentElement.parentElement;
+    listElement.remove();
+}
+
+function closePopUp(element) {
+    const alertContainer = element.parentElement.parentElement;
+    alertContainer.classList.add('inactive');
+}
