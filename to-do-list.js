f60a258418c67d665b608bab3078e41036208ffe
CraftPur-Official <rinku.apnatourandtravel@gmail.com>
2023-05-16 15:34:57 +0530

    final?

--- to-do-list.js
+++ to-do-list.js
@@ -1,6 +1,7 @@
 function addFunc(element) {
     const textField = element.previousElementSibling;
     const textFieldValue = textField.value;
+    const ulElement = document.querySelector('.myList');
     
     if(textFieldValue === '')
     {
@@ -8,7 +9,6 @@ function addFunc(element) {
         alertPopUp.classList.remove('inactive');
     }
     else {
-        const ulElement = document.querySelector('.myList');
         const listElement = document.createElement('li');
         const checkList = document.createElement('i');
         const deleteIcon = document.createElement('i');
@@ -31,6 +31,10 @@ function addFunc(element) {
         ulElement.append(listElement);
 
         textField.value = "";
+        if(ulElement.parentElement.classList.contains('inactive'))
+        {
+            ulElement.parentElement.classList.remove('inactive');
+        }
     }
 }
 
@@ -45,8 +49,15 @@ function deleteThisList(element) {
     listElement.classList.remove('slide-right');
     listElement.classList.remove('checked');
     listElement.classList.add('delete-poof');
+    const ulElement = document.querySelector('.myList');
+    
     setTimeout(() => {
         listElement.remove();
+        if(ulElement.childElementCount === 0)
+        {
+            console.log(ulElement.childElementCount);
+            ulElement.parentElement.classList.add('inactive');
+        }
     }, 250);
 }
 
